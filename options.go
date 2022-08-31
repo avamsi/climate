3bac15f5aec9cf98d4579f8a2d369e41d220a0b6
Vamsi Avula <avamsi07@gmail.com>
2022-08-31 19:47:51 +0530

    add support for slice fields (and so, slice flags)

--- options.go
+++ options.go
@@ -1,6 +1,7 @@
 package eclipse
 
 import (
+	"encoding/csv"
 	"fmt"
 	"os"
 	"reflect"
@@ -64,6 +65,46 @@ func flagVar[T any](opts flagVarOpts[T]) {
 	opts.flagTVar((*T)(opts.ptr), name, shorthand, defaultValue, opts.usage)
 }
 
+func s2i64(s string) (int64, error) {
+	return strconv.ParseInt(s, 10, 64)
+}
+
+func s2u64(s string) (uint64, error) {
+	return strconv.ParseUint(s, 10, 64)
+}
+
+func s2f64(s string) (float64, error) {
+	return strconv.ParseFloat(s, 64)
+}
+
+func s2s(s string) (string, error) {
+	return s, nil
+}
+
+func toSlice[T any](s2t func(string) (T, error)) func(string) ([]T, error) {
+	return func(s string) ([]T, error) {
+		if s == "" {
+			return []T{}, nil
+		}
+		ts := []T{}
+		for _, ss := range strings.Split(s, ",") {
+			t, err := s2t(ss)
+			if err != nil {
+				return nil, err
+			}
+			ts = append(ts, t)
+		}
+		return ts, nil
+	}
+}
+
+func s2ss(s string) ([]string, error) {
+	if s == "" {
+		return []string{}, nil
+	}
+	return csv.NewReader(strings.NewReader(s)).Read()
+}
+
 func (opts *options) declareFlags(fs *flag.FlagSet, parentID string) {
 	for i := 0; i < opts.t.NumField(); i++ {
 		ptr := opts.v.Field(i).Addr().UnsafePointer()
@@ -73,23 +114,27 @@ func (opts *options) declareFlags(fs *flag.FlagSet, parentID string) {
 		case reflect.Bool:
 			flagVar(flagVarOpts[bool]{fs.BoolVarP, ptr, sf, strconv.ParseBool, usage})
 		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-			s2i64 := func(s string) (int64, error) {
-				return strconv.ParseInt(s, 10, 64)
-			}
 			flagVar(flagVarOpts[int64]{fs.Int64VarP, ptr, sf, s2i64, usage})
 		case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
-			s2u64 := func(s string) (uint64, error) {
-				return strconv.ParseUint(s, 10, 64)
-			}
 			flagVar(flagVarOpts[uint64]{fs.Uint64VarP, ptr, sf, s2u64, usage})
 		case reflect.Float32, reflect.Float64:
-			s2f64 := func(s string) (float64, error) {
-				return strconv.ParseFloat(s, 64)
-			}
 			flagVar(flagVarOpts[float64]{fs.Float64VarP, ptr, sf, s2f64, usage})
 		case reflect.String:
-			s2s := func(s string) (string, error) { return s, nil }
 			flagVar(flagVarOpts[string]{fs.StringVarP, ptr, sf, s2s, usage})
+		case reflect.Slice:
+			switch sf.Type.Elem().Kind() {
+			case reflect.Bool:
+				flagVar(flagVarOpts[[]bool]{fs.BoolSliceVarP, ptr, sf, toSlice(strconv.ParseBool), usage})
+			case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+				flagVar(flagVarOpts[[]int64]{fs.Int64SliceVarP, ptr, sf, toSlice(s2i64), usage})
+			case reflect.Float32, reflect.Float64:
+				flagVar(flagVarOpts[[]float64]{fs.Float64SliceVarP, ptr, sf, toSlice(s2f64), usage})
+			case reflect.String:
+				flagVar(flagVarOpts[[]string]{fs.StringSliceVarP, ptr, sf, toSlice(s2s), usage})
+			default:
+				fmt.Fprintf(os.Stderr, "got: '%#v'; want: []bool|[]int|[]float|[]string fields\n", sf)
+				os.Exit(1)
+			}
 		case reflect.Struct:
 			if opts.parentIndex != -1 {
 				fmt.Fprintf(os.Stderr, "got: '%#v'; want: exactly one struct field\n", sf)
@@ -97,7 +142,7 @@ func (opts *options) declareFlags(fs *flag.FlagSet, parentID string) {
 			}
 			opts.parentIndex = i
 		default:
-			fmt.Fprintf(os.Stderr, "got: '%#v'; want: bool|int|uint|float|string fields\n", sf)
+			fmt.Fprintf(os.Stderr, "got: '%#v'; want: bool|int|uint|float|string|slice fields\n", sf)
 			os.Exit(1)
 		}
 	}
